9e0d8340f74ec6abd10666756fcd55046cc8db2d
Aditya Wibisana <aditya.wibisana@gmail.com>
2022-11-30 12:38:18 +0700

    Force to use goerli

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,6 @@
-export const ensRegistrarAddr = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'
+// export const ensRegistrarAddr = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5' // Mainnet
+export const ensRegistrarAddr = '0x283af0b28c62c092c9727f1ee09c02ca627eb7f5' // goerli
+
 export const ensBaseRegistrarAddr = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85'
 
 export const ensRegistrarAbi = require('./ens-registrar-abi.json')
@@ -12,4 +14,4 @@ export const ensRegistrarConfig = {
 }
 
 export const ensResolver = '0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41'
-export const ensResolverRinkeby = '0xf6305c19e814d2a75429Fd637d01F7ee0E77d615'
+export const ensResolverGoerli = '0x840e870459a2c960d70ede14d6daf212c37429c6'
